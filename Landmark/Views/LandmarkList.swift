e89c14f3b8e366e2fd7ec57ed77958089fa17816
thinkySide <113565086+thinkySide@users.noreply.github.com>
2023-07-21 15:45:21 +0900

    Complete - Building Lists and Navigation

--- Landmark/Views/LandmarkList.swift
+++ Landmark/Views/LandmarkList.swift
@@ -33,7 +33,7 @@ struct LandmarkList: View {
             List(landmarks) { landmark in
                 NavigationLink {
                     // Destination
-                    LandmarkDetail()
+                    LandmarkDetail(landmark: landmark)
                 } label: {
                     LandmarkRow(landmark: landmark)
                 }
@@ -45,6 +45,25 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        
+        let devices = [
+            "iPhone 14 Pro",
+            "iPhone SE (3rd generation)",
+            "iPad Air (5th generation)",
+        ]
+        
+        // \.self 키워드??
+        // -> Collection에서 각 항목을 나타내는 KeyPath값을 지정해주기 위해 사용.
+        // -> 각 항목이 자동으로 유일한 식별자로 사용됨.
+        // -> 간단한 값들을 표시할 때 유용하게 사용.
+        // 아래에서는 devices 배열의 각 요소를 식별하고 있음.
+        
+        ForEach(devices, id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        
+        // 상단 Scheme 메뉴에서 rawValue 확인이 가능하다. (없는 건 안댐)
     }
 }
--- Landmark/Views/MapView.swift
+++ Landmark/Views/MapView.swift
@@ -10,6 +10,8 @@ import MapKit
 
 struct MapView: View {
     
+    var coordinate: CLLocationCoordinate2D
+    
     // View를 자동으로 업데이트 시킬 수 있는 @State 어트리뷰트
     
     @State private var region = MKCoordinateRegion(
@@ -24,11 +26,23 @@ struct MapView: View {
         // 기본값에 대한 참조와 같습니다.
         
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: CLLocationCoordinate2D(
+            latitude: 34.011_286, longitude: -116.166_868)
+        )
     }
 }
