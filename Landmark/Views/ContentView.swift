6cd2c9e3de4cb0ba8bf06bc93c772c085584dc86
thinkySide <113565086+thinkySide@users.noreply.github.com>
2023-07-21 15:45:58 +0900

    Complete - Building Lists and Navigation

--- Landmark/Views/ContentView.swift
+++ Landmark/Views/ContentView.swift
@@ -10,54 +10,13 @@ import SwiftUI
 // 콘텐츠와 레이아웃 구성을 위한 구조체
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-                .ignoresSafeArea(edges: .top)
-            
-            // height 파라미터만 지정해주면, 뷰가 자동으로
-            // 콘텐츠 너비에 맞게 width 사이즈를 잡아줌.
-            
-            CircleImage()
-                .offset(y: -190)
-                .padding(.bottom, -190)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                
-                HStack {
-                    Text("Joshua Tree National Park")
-                    
-                    Spacer()
-                    // Spacer의 특징
-                    // 1. 다른 콘텐츠에 의해서만 크기가 정의됨.
-                    // 2. 상위 뷰의 모든 공간을 사용하도록 확장됨.
-                    
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                // Stack에 Modifier를 적용시키면 그 안에 있는 모든 요소에 적용됨.
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                
-                Text("Desciptive text goes here.")
-            }
-            .padding()
-            
-            Spacer()
-        }
-        
-        // SwiftUI View를 커스터마이징 하기 위해 사용하는 메서드
-        // -> Modifiers
-        
-        // 각각의 Modifier는 새로운 View를 반환한다.
+        LandmarkList()
     }
+    
+    // SwiftUI View를 커스터마이징 하기 위해 사용하는 메서드
+    // -> Modifiers
+    
+    // 각각의 Modifier는 새로운 View를 반환한다.
 }
 // body 프로퍼티는 단일 View만 반환한다.
 
---
+++ Landmark/Views/LandmarkDetail.swift
@@ -0,0 +1,66 @@
+//
+//  LandmarkDetail.swift
+//  Landmark
+//
+//  Created by 김민준 on 2023/07/21.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            // height 파라미터만 지정해주면, 뷰가 자동으로
+            // 콘텐츠 너비에 맞게 width 사이즈를 잡아줌.
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -190)
+                .padding(.bottom, -190)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                
+                HStack {
+                    Text(landmark.park)
+                    
+                    Spacer()
+                    // Spacer의 특징
+                    // 1. 다른 콘텐츠에 의해서만 크기가 정의됨.
+                    // 2. 상위 뷰의 모든 공간을 사용하도록 확장됨.
+                    
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                // Stack에 Modifier를 적용시키면 그 안에 있는 모든 요소에 적용됨.
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+        
+        // Navigation 관련 코드는 Navigation Stack이 쌓일 때만 확인 가능하다.
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
