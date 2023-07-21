3c337b5c75a5c99b2c38b5e265fe9afaef0fd0d8
thinkySide <113565086+thinkySide@users.noreply.github.com>
2023-07-21 13:24:10 +0900

    Building List and Navigation - Section1

---
+++ Landmark/Model/ModelData.swift
@@ -0,0 +1,30 @@
+//
+//  ModelData.swift
+//  Landmark
+//
+//  Created by 김민준 on 2023/07/21.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ fileName: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: fileName, withExtension: nil)
+    else { fatalError("Couldn't find \(fileName) in main Bundle") }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(fileName) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(fileName) as \(T.self):\n\(error)")
+    }
+}
